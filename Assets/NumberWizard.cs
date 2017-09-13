16b73970dcc32f448718c21f28e34060098c9495
Amanda <amanda.iaria@gmail.com>
2017-09-13 16:25:58 -0400

    Finished the game, and added the build version.

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+	int max;
+	int min;
+	int guess;
+	
+	public int maxGuessesAllowed = 10;
+	public Text text;
+
+	void Start () {
+		StartGame();
+	}
+	
+	
+	public void GuessHigher(){
+		min = guess;
+		NextGuess();
+	}
+	
+	public void GuessLower(){
+		max = guess;
+		NextGuess();
+	}
+	
+	
+	void StartGame () {
+		max = 1000;
+		min = 1;
+		NextGuess ();
+	}
+	
+	void NextGuess() {
+		guess = Random.Range(min , max +1 );
+		text.text = guess.ToString();
+		maxGuessesAllowed = maxGuessesAllowed - 1;
+		if(maxGuessesAllowed <= 0){
+			Application.LoadLevel("Win");
+		}
+	}
+}
